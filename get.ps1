3606ffed2a9116c18b082ca1b402f61968ab9efb
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-01-23 23:33:55 +0200

    update

--- get.ps1
+++ get.ps1
@@ -2,38 +2,40 @@
 $releaseUrl = "https://github.com/refa3211/nextdns/files/14027656/nextdns_1.41.0_windows_amd64_2.zip"  # Replace with the actual release URL
 
 # Determine the appropriate directory based on admin privileges
-$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\nextdns" } else { "$env:TEMP\nextdns" }
-
-try {
-    # Download the zip file
-    Invoke-WebRequest -Uri $releaseUrl -OutFile "$FilePath.zip"
+$FilePath = if ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") {
+    "$env:SystemRoot\Temp\nextdns"
+} else {
+    "$env:TEMP\nextdns"
 }
-catch {
+
+# Self-elevate the script if not already running as admin
+If (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+# Download the zip file
+try {
+    Invoke-WebRequest -Uri $releaseUrl -OutFile "$FilePath.zip"
+} catch {
     Write-Error "Failed to download the ZIP file from GitHub. $_"
     exit 1
 }
 
+# Unzip the contents to the specified path
 try {
-    # Unzip the contents to the specified path
     Expand-Archive -Path "$FilePath.zip" -DestinationPath $FilePath -Force
-
-    # Clean up: Optionally, you can remove the ZIP file after extraction
-    Remove-Item -Path "$FilePath.zip" -Force
-
-    # Self-elevate the script if required
-    If (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-        {
-        $arguments = "Invoke-RestMethod https://raw.githubusercontent.com/refa3211/nextdns/main/get.ps1 | Invoke-Expression"
-        Start-Process powershell -Verb runAs -ArgumentList $arguments
-        Break
-        }
-
-    # Run the command with elevated permissions
-    & "$FilePath\nextdns.exe" install -profile 159376 -auto-activate -report-client-info
+    Remove-Item -Path "$FilePath.zip" -Force  # Clean up the ZIP file
+} catch {
+    Write-Error "Error extracting the ZIP file: $_"
+    exit 1
 }
-catch {
+
+# Run the command with elevated permissions
+try {
+    Start-Process -FilePath "$FilePath\nextdns.exe" -ArgumentList "install -profile 159376 -auto-activate -report-client-info" -Verb RunAs
+} catch {
     Write-Error "Error running nextdns: $_"
+    exit 1
 }
-
-# Optionally, you can add more cleanup or further actions here
