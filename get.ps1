0867aaec0f679f21daf40e7ac9708cb304e69717
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-01-23 19:53:42 +0200

    update

--- get.ps1
+++ get.ps1
@@ -1,20 +1,17 @@
 # Specify the GitHub repository URL and files to download
 Write-Output "This is a message."
 
-
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 $releaseUrl = "https://github.com/refa3211/nextdns/files/14027656/nextdns_1.41.0_windows_amd64_2.zip"  # Replace with the actual release URL
-# $zipFilePath = "$env:TEMP\nextdns.zip"
-# $extractPath = "$env:TEMP\nextdns"
 
+# Determine the appropriate directory based on admin privileges
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\nextdns" } else { "$env:TEMP\nextdns" }
 
 try {
     Invoke-WebRequest -Uri $releaseUrl -OutFile "$FilePath.zip"
-
 }
 catch {
     Write-Error "Failed to download files from GitHub. $_"
@@ -28,21 +25,26 @@ Expand-Archive -Path "$FilePath.zip" -DestinationPath $FilePath -Force
 Remove-Item -Path "$FilePath.zip"
 
 try {
-    # Self-elevate the script if require
+    # Self-elevate the script if required
     if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
         if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-            $CommandLine = Invoke-RestMethod https://raw.githubusercontent.com/refa3211/nextdns/main/get.ps1 | Invoke-Expression
-            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+            $CommandLine = "& `"$($MyInvocation.InvocationName)`" $args"
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.InvocationName)`" $args"
             Exit
         }
     }
 
-    [System.Security.Principal.WindowsBuiltInRole]::Administrator
-    & $env:TEMP\nextdns\nextdns.exe install -profile 159376 -auto-activate -report-client-info
-    
+    # Check if the current session is elevated
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        Write-Error "The script is not running with administrator privileges."
+        Exit
+    }
+
+    # Run the command with elevated permissions
+    & "$FilePath\nextdns.exe" install -profile 159376 -auto-activate -report-client-info
 }
 catch {
-    Write-Error "error running the nextdns $_"
+    Write-Error "Error running nextdns: $_"
 }
 
 # Check the status of the service
