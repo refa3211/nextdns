b5c695fc659d52a3dcac46f0f9692f313026cbe3
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-01-23 20:09:49 +0200

    update

--- get.ps1
+++ get.ps1
@@ -1,9 +1,4 @@
 # Specify the GitHub repository URL and files to download
-Write-Output "This is a message."
-
-# Enable TLSv1.2 for compatibility with older clients
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
 $releaseUrl = "https://github.com/refa3211/nextdns/files/14027656/nextdns_1.41.0_windows_amd64_2.zip"  # Replace with the actual release URL
 
 # Determine the appropriate directory based on admin privileges
@@ -11,25 +6,26 @@ $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -mat
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\nextdns" } else { "$env:TEMP\nextdns" }
 
 try {
+    # Download the zip file
     Invoke-WebRequest -Uri $releaseUrl -OutFile "$FilePath.zip"
 }
 catch {
-    Write-Error "Failed to download files from GitHub. $_"
+    Write-Error "Failed to download the ZIP file from GitHub. $_"
     exit 1
 }
 
-# Unzip the contents to the specified path
-Expand-Archive -Path "$FilePath.zip" -DestinationPath $FilePath -Force
-
-# Clean up: Optionally, you can remove the ZIP file after extraction
-Remove-Item -Path "$FilePath.zip"
-
 try {
+    # Unzip the contents to the specified path
+    Expand-Archive -Path "$FilePath.zip" -DestinationPath $FilePath -Force
+
+    # Clean up: Optionally, you can remove the ZIP file after extraction
+    Remove-Item -Path "$FilePath.zip" -Force
+
     # Self-elevate the script if required
     if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
         $ScriptPath = $MyInvocation.MyCommand.Definition
         $CommandLine = "& `"$ScriptPath`" $args"
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "irm https://raw.githubusercontent.com/refa3211/nextdns/main/get.ps1 | iex"
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$ScriptPath`" $args"
         Exit
     }
 
@@ -46,8 +42,4 @@ catch {
     Write-Error "Error running nextdns: $_"
 }
 
-# Check the status of the service
-Get-Service nextdns
-
-# Keep the PowerShell window open
-Pause
+# Optionally, you can add more cleanup or further actions here
