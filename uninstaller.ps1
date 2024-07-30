04a5e7477be421e89d2f5767bac65f6cff48441a
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-07-30 13:30:02 +0300

    Create uninstaller.ps1

---
+++ uninstaller.ps1
@@ -0,0 +1,39 @@
+# Define the installation path
+$FilePath = "$env:TEMP\nextdns"
+
+# Function to remove the installed certificate
+function RemoveCertificate {
+    try {
+        $cert = Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object { $_.Subject -like "*NextDNS*" }
+        if ($cert) {
+            Remove-Item -Path $cert.PSPath -Force
+            Write-Output "NextDNS certificate removed successfully."
+        } else {
+            Write-Output "NextDNS certificate not found."
+        }
+    } catch {
+        Write-Output "Failed to remove NextDNS certificate: $_"
+    }
+}
+
+# Uninstall NextDNS
+try {
+    Start-Process -FilePath "$FilePath\nextdns.exe" -ArgumentList "uninstall" -Verb RunAs -Wait
+    Write-Output "NextDNS uninstalled successfully."
+} catch {
+    Write-Error "Error uninstalling NextDNS: $_"
+}
+
+# Remove the certificate
+RemoveCertificate
+
+# Remove the downloaded files
+try {
+    Remove-Item -Path $FilePath -Recurse -Force
+    Remove-Item -Path "$env:TEMP\cer.cer" -Force -ErrorAction SilentlyContinue
+    Write-Output "Temporary files removed successfully."
+} catch {
+    Write-Error "Error removing temporary files: $_"
+}
+
+Write-Output "Uninstallation process completed."
